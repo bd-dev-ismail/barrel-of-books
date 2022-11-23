93eb4f978bb8fd67c9cf78b31ceb84bc42b2b279
bd-dev-ismail <ismailratul7@gmail.com>
2022-11-23 22:45:58 +0600

    folder setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        booksthemes: {
+          primary: "#62ab00",
+
+          secondary: "#bd0018",
+
+          accent: "#37CDBE",
+
+          neutral: "#3D4451",
+
+          "base-100": "#FFFFFF",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
