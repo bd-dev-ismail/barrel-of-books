35904a5b6fd8e8ee93702adee2bded7fb9850392
bd-dev-ismail <ismailratul7@gmail.com>
2022-11-24 13:02:39 +0600

    create authcontext

--- src/index.js
+++ src/index.js
@@ -8,13 +8,16 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import AuthProvider from './context/AuthProvider/AuthProvider';
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
