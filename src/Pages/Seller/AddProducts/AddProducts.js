6f1ca72fca7346048c152ffc571386d6926aa9d9
bd-dev-ismail <ismailratul7@gmail.com>
2022-11-24 09:34:06 +0600

    added add proudct form

--- src/Pages/Seller/AddProducts/AddProducts.js
+++ src/Pages/Seller/AddProducts/AddProducts.js
@@ -3,7 +3,169 @@ import React from 'react';
 const AddProducts = () => {
     return (
       <div>
-        <h3 className="text-3xl font-semibold my-5">Add Products</h3>
+        {/* <h3 className="text-3xl font-semibold my-3">Add Products</h3> */}
+        <div>
+          {/* <!-- component --> */}
+          <section class="max-w-4xl p-6 mx-auto bg-primary rounded-md shadow-md dark:bg-gray-800 mt-10">
+            <h1 class="text-xl font-bold text-white capitalize dark:text-white">
+              Add Products
+            </h1>
+            <form>
+              <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                <div>
+                  <label class="text-white dark:text-gray-200" for="username">
+                    Product Name
+                  </label>
+                  <input
+                    id="username"
+                    type="text"
+                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  />
+                </div>
+
+                <div>
+                  <label
+                    class="text-white dark:text-gray-200"
+                    for="emailAddress"
+                  >
+                    Product Price
+                  </label>
+                  <input
+                    id="emailAddress"
+                    type="email"
+                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  />
+                </div>
+                <div>
+                  <label class="text-white dark:text-gray-200" for="email">
+                    Seller Email
+                  </label>
+                  <input
+                    id="email"
+                    type="email"
+                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  />
+                </div>
+                <div>
+                  <label
+                    class="text-white dark:text-gray-200"
+                    for="mobileNumber"
+                  >
+                    Seller Phone
+                  </label>
+                  <input
+                    id="mobileNumber"
+                    type="number"
+                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  />
+                </div>
+                <div>
+                  <label
+                    class="text-white dark:text-gray-200"
+                    for="passwordConfirmation"
+                  >
+                    Location
+                  </label>
+                  <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                    <option>Chattogram</option>
+                    <option>Dhaka</option>
+                    <option>Kulna</option>
+                    <option>Syhlet</option>
+                    <option>Barishal</option>
+                    <option>Noakhali</option>
+                  </select>
+                </div>
+
+                <div>
+                  <label
+                    class="text-white dark:text-gray-200"
+                    for="passwordConfirmation"
+                  >
+                    Product Condition
+                  </label>
+                  <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                    <option>Excellent</option>
+                    <option>Good</option>
+                    <option>Fair</option>
+                  </select>
+                </div>
+                <div>
+                  <label
+                    class="text-white dark:text-gray-200"
+                    for="passwordConfirmation"
+                  >
+                    Year Of Purchase
+                  </label>
+                  <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                    <option>2018</option>
+                    <option>2019</option>
+                    <option>2020</option>
+                    <option>2021</option>
+                    <option>2022</option>
+                  </select>
+                </div>
+
+                <div>
+                  <label
+                    class="text-white dark:text-gray-200"
+                    for="passwordConfirmation"
+                  >
+                    Date
+                  </label>
+                  <input
+                    id="date"
+                    type="date"
+                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  />
+                </div>
+                <div>
+                  <label class="text-white dark:text-gray-200" for="desc">
+                    Product Description
+                  </label>
+                  <textarea
+                    id="desc"
+                    type="textarea"
+                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  ></textarea>
+                </div>
+                <div>
+                  <label class="block text-sm font-medium text-white">
+                    Product Image
+                  </label>
+                  <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                    <div class="space-y-1 text-center">
+                      <div class="flex text-sm text-gray-600">
+                        <label
+                          for="file-upload"
+                          class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                        >
+                          <span class="px-4">Upload a file</span>
+                          <input
+                            id="file-upload"
+                            name="file-upload"
+                            type="file"
+                            class="sr-only"
+                          />
+                        </label>
+                        <p class="pl-1 text-white">Accept Image type</p>
+                      </div>
+                      <p class="text-xs text-white">
+                        PNG, JPG, WEBP up to 10MB
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="flex justify-end mt-6">
+                <button class="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600">
+                  Save
+                </button>
+              </div>
+            </form>
+          </section>
+
+        </div>
       </div>
     );
 };
